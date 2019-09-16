6d71bd92db9e75975a9a4cc68f81753faf7a1a62
Ian Roskow <ianroskow@gmail.com>
2019-09-16 23:54:18 +0200

    Added middle ware

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,10 +1,36 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
 
-//@route    Get api/users
-//@desc     Test route
+const user = require('../../models/User');
+
+//@route    Post api/users
+//@desc     Register user
 //@access   Public
 
-router.get('/', (req, res) => res.send('User route'));
+router.post(
+  '/',
+  [
+    check('name', 'Name is required').isLength({ min: 1 }),
+    check('email', 'Please include a valid email').isEmail(),
+    check(
+      'password',
+      'Please enter a password with 6 or more characters'
+    ).isLength({ min: 6 })
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { name, email, password } = req.body;
+
+    try {
+    } catch (err) {}
+
+    res.send('User route');
+  }
+);
 
 module.exports = router;
