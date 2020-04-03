9235852c152ff484baaef359e280b102bff411ed
Ian Roskow <ianroskow@gmail.com>
2020-04-03 17:03:48 +0200

    Added comment routes to the post api.

--- routes/api/post.js
+++ routes/api/post.js
@@ -10,7 +10,6 @@ const Profile = require('../../models/Profile');
 //@route    POST api/posts
 //@desc     Create a post
 //@access   Private
-
 router.post(
   '/',
   [
@@ -26,15 +25,6 @@ router.post(
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-
-    const user = await User.findById(req.user.id).select('-password');
-
-    const newPost = {
-      text: req.body.text,
-      name: user.name,
-      avatar: user.avatar,
-      user: req.user.id
-    };
 
     try {
       const user = await User.findById(req.user.id).select('-password');
@@ -190,6 +180,85 @@ router.put('/unlike/:id', auth, async (req, res) => {
     if (err.kind === 'ObjectId') {
       return res.status(404).json({ msg: 'Post not found.' });
     }
+    res.status(500).send('Server Error');
+  }
+});
+
+//@route    POST api/posts/comment/:id
+//@desc     Comment on a post
+//@access   Private
+router.post(
+  '/comment/:id',
+  [
+    auth,
+    [
+      check('text', 'Text is required')
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const user = await User.findById(req.user.id).select('-password');
+      const post = await Post.findById(req.params.id);
+
+      const newComment = {
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id
+      };
+
+      post.comments.unshift(newComment);
+
+      await post.save();
+
+      res.json(post.comments);
+    } catch (err) {
+      console.log(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
+
+//@route    Delete api/posts/comment/:id/:comment_id
+//@desc     Delete comment from a post
+//@access   Private
+router.delete('/comment/:id/:comment_id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    //Pull out comment from the post
+    const comment = post.comments.find(
+      comment => comment.id === req.params.comment_id
+    );
+
+    //Make sure the comment exists
+    if (!comment) {
+      return res.status(404).json({ msg: 'Comment does not exist' });
+    }
+
+    //check user
+    if (comment.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: 'User not authorized' });
+    }
+
+    //Remove the comment from the comments
+    post.comments = post.comments.filter(
+      ({ id }) => id !== req.params.comment_id
+    );
+
+    //save the post with the updated likes
+    await post.save();
+    //return the total likes for front end logic
+    res.json(post.comments);
+  } catch (err) {
+    console.log(err.message);
     res.status(500).send('Server Error');
   }
 });
