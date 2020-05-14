dab8df97f8f77e57ec9cdcdd62602b802140412a
Ian Roskow <ianroskow@gmail.com>
2020-05-14 09:09:14 +0200

    Fixed font awesome icons and started on ui for dashboard

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -8,8 +8,14 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
   const authLinks = (
     <ul>
       <li>
+        <Link to='/dashboard'>
+          <i className='fa fa-user'></i>{' '}
+          <span className='hide-sm'>Dashboard</span>
+        </Link>
+      </li>
+      <li>
         <a onClick={logout} href='#!'>
-          <i className='fas fa-sign-out-alt'></i>{' '}
+          <i className='fa fa-sign-out'></i>{' '}
           <span className='hide-sm'>Logout</span>
         </a>
       </li>
@@ -34,7 +40,7 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
     <li className='navbar bg-dark'>
       <h1>
         <Link to='/'>
-          <i className='fas fa-code'></i> DevConnector
+          <i className='fa fa-code'></i> DevConnector
         </Link>
       </h1>
       {!loading && (
