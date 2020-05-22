9037358246a2c7b2c3451af64f0845829dfee416
Ian Roskow <ianroskow@gmail.com>
2020-05-22 11:37:49 +0200

    Bug fixes

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -33,12 +33,9 @@ const Profile = ({
           {auth.isAuthenticated &&
             auth.loading === false &&
             auth.user._id === profile.user._id && (
-              <Link
-                to='edit-profile'
-                className='btn btn-dark'
-                Edit
-                Profile
-              ></Link>
+              <Link to='/edit-profile' className='btn btn-dark'>
+                EditProfile
+              </Link>
             )}
           <div className='profile-grid my-1'>
             <ProfileTop profile={profile} />
